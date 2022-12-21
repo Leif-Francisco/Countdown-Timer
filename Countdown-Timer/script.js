822c83d58675905a5b7ada96c82b311ed336f625
Leif-Francisco <105963035+Leif-Francisco@users.noreply.github.com>
2022-12-21 08:29:21 +0800

    Finished Javascript

--- Countdown-Timer/script.js
+++ Countdown-Timer/script.js
@@ -1,1 +1,29 @@
-s
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours')
+const minutesEl = document.getElementById('minutes')
+const secondsEl = document.getElementById('seconds')
+
+
+const christmas = "25 Dec 2022";
+
+function countDown () {
+    const christmasDate = new Date(christmas);
+    const currentDate = new Date();
+
+    const totalSeconds = (christmasDate - currentDate)/1000;
+
+    const days = Math.floor(totalSeconds/3600/24);
+    const hours = Math.floor(totalSeconds/3600) % 24;
+    const minutes = Math.floor(totalSeconds/60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minutesEl.innerHTML = minutes;
+    secondsEl.innerHTML = seconds;
+
+}
+
+countDown();
+
+setInterval(countDown,1000);
